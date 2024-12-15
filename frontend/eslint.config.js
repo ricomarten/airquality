644568d296e578279d4551a4ac7d5b8acc598cd3
Rico Martenstyaro <ricomarten@gmail.com>
2024-12-15 23:32:48 +0700

    simpan

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,6 +5,13 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
+  {
+    build: {
+      rollupOptions: {
+        external: ['axios'],
+      },
+    },
+  },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
